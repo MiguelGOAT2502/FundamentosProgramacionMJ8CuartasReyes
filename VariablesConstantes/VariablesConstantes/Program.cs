ca5cb805970d107df19c281ff2ce769cc29d90f7
MiguelGOAT2502 <miguel.cuartasr@upb.edu.co>
2026-07-30 08:15:13 -0500

    Primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui va el codigo fuente
+            //Variables
+            string nombre = "Miguel";
+            Console.WriteLine("Bienvenido " + nombre + " al curso de Fundamentos de Programacion");
+        }
+    }
+}
