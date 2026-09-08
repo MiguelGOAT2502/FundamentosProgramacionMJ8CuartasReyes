ee86b589a5806066c3478b87b65693fe1764ec98
MiguelGOAT2502 <miguel.cuartasr@upb.edu.co>
2026-09-08 08:30:29 -0500

    13.CicloFor1

---
+++ 13.CicloFor1/13.CicloFor1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _13.CicloFor1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un programa que permita imprimir por pantalla los números múltiplos de 5, el usuario debe ingresar por el teclado el rango de números a evaluar.
+            
+            int acumulador = 0;
+            int rango;
+
+            Console.WriteLine("ingresa la cantidad de multiplos de 5 que quieres ver: " );
+           rango = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= rango; i++)
+            {
+                acumulador = i * 5;
+                Console.WriteLine(acumulador);
+            }
+
+        }
+    }
+}
